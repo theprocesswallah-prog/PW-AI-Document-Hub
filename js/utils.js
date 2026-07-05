5aef2930e666c6564c49f8a02759b2d97dd08cd7
theprocesswallah-prog <theprocesswallah@gmail.com>
2026-07-05 22:01:49 +0530

    Add utility functions for currency and date formatting

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,18 @@
+/**
+ * System-Wide Utilities
+ */
+const Utils = {
+    formatCurrency(value, currency = 'USD') {
+        return new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: currency
+        }).format(value);
+    },
 
+    formatDate(dateString) {
+        const options = { year: 'numeric', month: 'short', day: 'numeric' };
+        return new Date(dateString).toLocaleDateString(undefined, options);
+    }
+};
+
+window.AppUtils = Utils;
